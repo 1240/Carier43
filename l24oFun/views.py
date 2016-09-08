71a25afb8894432762576139a7269d0617b28bb9
Alexander Popov <chuffey.1240@yandex.ru>
2016-09-08 17:12:12 +0300

    some refactor

--- l24oFun/views.py
+++ l24oFun/views.py
@@ -8,17 +8,17 @@ from l24oFun.models import Product, ContactForm
 
 
 def index(request):
+    global for_thanks
     products = Product.objects.all()
 
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        body = " Номер телефона: {0} \n Продукт: {1} \n Кол-во: {2}".format(form.cleaned_data['phone_number'],
-                                                                            form.cleaned_data['product'],
-                                                                            form.cleaned_data['count'])
-        email = EmailMessage("Заявка на сайте 'гравийпесок.рф'", body, "carierkirov43@gmail.com",
-                             to=["chuffey.1240@ya.ru"])
-        email.send()
-        for_thanks = True
+        if form.is_valid():
+            body = " Номер телефона: {0} \n Продукт: {1} \n Кол-во: {2}".format(form.cleaned_data['phone_number'], form.cleaned_data['product'], form.cleaned_data['count'])
+            email = EmailMessage("Заявка на сайте 'гравийпесок.рф'", body, "carierkirov43@gmail.com",
+                                 to=["chuffey.1240@ya.ru"])
+            email.send()
+            for_thanks = True
     else:
         for_thanks = False
         form = ContactForm()
